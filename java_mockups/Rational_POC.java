b9544a181775022969e0dedc9ec961a359223652
Rosie Bicknell <bicro435@student.otago.ac.nz>
2023-05-12 21:03:40 +1200

    rational wrapper class

--- java_mockups/Rational_POC.java
+++ java_mockups/Rational_POC.java
@@ -2,32 +2,60 @@ import java.math.BigInteger;
 
 public class Rational_POC {
     public static void main(String[] args) {
-        BigInteger num1 = new BigInteger("5");
-        BigInteger num2 = new BigInteger("2");
-        BigInteger num3 = new BigInteger("4");
-        BigInteger num4 = new BigInteger("0");
-        Rational_Wrapper real1 = new Rational_Wrapper(num1, num2);
-        Rational_Wrapper real2 = new Rational_Wrapper(num1);
-        Rational_Wrapper real3 = new Rational_Wrapper(num1, num2, num3);
-        Rational_Wrapper real4 = new Rational_Wrapper(num3, num2);
-        Rational_Wrapper real5 = new Rational_Wrapper(num2, num1);
-        Rational_Wrapper real6 = new Rational_Wrapper(num4);
-        // Rational_Wrapper real7 = new Rational_Wrapper(num1, num4); //throws exception
-        Rational_Wrapper real8 = new Rational_Wrapper(num4, num1, num2);
-        Rational_Wrapper real9 = new Rational_Wrapper(num1, num4, num2);
-        // Rational_Wrapper real10 = new Rational_Wrapper(num1, num2, num4); // throws
+        // BigInteger num1 = new BigInteger("5");
+        // BigInteger num2 = new BigInteger("2");
+        // BigInteger num3 = new BigInteger("4");
+        // BigInteger num4 = new BigInteger("0");
+        // Rational_Wrapper real1 = new Rational_Wrapper(num1, num2);
+        // Rational_Wrapper real2 = new Rational_Wrapper(num1);
+        // Rational_Wrapper real3 = new Rational_Wrapper(num1, num2, num3);
+        // Rational_Wrapper real4 = new Rational_Wrapper(num3, num2);
+        // Rational_Wrapper real5 = new Rational_Wrapper(num2, num1);
+        // Rational_Wrapper real6 = new Rational_Wrapper(num4);
+        // // Rational_Wrapper real7 = new Rational_Wrapper(num1, num4); //throws
         // exception
-        Rational_Wrapper real11 = new Rational_Wrapper(num4, num1);
-        System.out.println(real1);
-        System.out.println(real2);
-        System.out.println(real3);
-        System.out.println(real4);
-        System.out.println(real5);
-        System.out.println(real6);
-        // System.out.println(real7);
-        System.out.println(real8);
-        System.out.println(real9);
-        // System.out.println(real10);
-        System.out.println(real11);
+        // Rational_Wrapper real8 = new Rational_Wrapper(num4, num1, num2);
+        // Rational_Wrapper real9 = new Rational_Wrapper(num1, num4, num2);
+        // // Rational_Wrapper real10 = new Rational_Wrapper(num1, num2, num4); //
+        // throws
+        // Rational_Wrapper real11 = new Rational_Wrapper(num4, num1);
+        // // exception
+
+        // // Testing printing out numbers
+
+        // System.out.println(real1);
+        // System.out.println(real2);
+        // System.out.println(real3);
+        // System.out.println(real4);
+        // System.out.println(real5);
+        // System.out.println(real6);
+        // // // System.out.println(real7);
+        // System.out.println(real8);
+        // System.out.println(real9);
+        // // // System.out.println(real10);
+        // System.out.println(real11);
+
+        // // testing comparison
+        // System.out.println(real2.isGreaterThan(real9));
+        // System.out.println(real2.isLessThan(real9)); //
+        // System.out.println(real2.equals(real9)); //
+
+        // testing addition
+        Rational_Wrapper real1 = new Rational_Wrapper(new BigInteger("-2"), new BigInteger("3"));
+        Rational_Wrapper real2 = new Rational_Wrapper(new BigInteger("-5"), new BigInteger("7"));
+
+        // printing out the numbers
+        System.out.println("Number 1: " + real1);
+        System.out.println("Number 2: " + real2);
+
+        // addition
+        System.out.println(Rational_Wrapper.add(real1, real2));
+        // subtraction
+        System.out.println(Rational_Wrapper.subtract(real1, real2));
+        // multiplication
+        System.out.println(Rational_Wrapper.multiply(real1, real2));
+        // division
+        System.out.println(Rational_Wrapper.divide(real1, real2));
+
     }
 }
--- java_mockups/Rational_Wrapper.java
+++ java_mockups/Rational_Wrapper.java
@@ -12,6 +12,9 @@ public class Rational_Wrapper {
 
     // consetructor which takes in a rational number with numerator and denominator
     public Rational_Wrapper(BigInteger n, BigInteger d) {
+        // testing
+        System.out.println("Standardised: " + n + "/" + d);
+
         // check that denominator doesn't equal 0
 
         if (d.equals(new BigInteger("0"))) {
@@ -20,9 +23,13 @@ public class Rational_Wrapper {
             numerator = n;
             denimonator = d;
         }
+
+        standardizeSigns();
+
     }
 
     public Rational_Wrapper(BigInteger w, BigInteger n, BigInteger d) {
+
         if (d.equals(new BigInteger("0"))) {
             throw new ArithmeticException("Cannot have 0 as a deniminator");
         } else {
@@ -33,18 +40,114 @@ public class Rational_Wrapper {
         numerator = w.multiply(d).add(n); // w.n/d multiply numerator by the denominator and then add it to the current
                                           // numerator
         denimonator = d;
+
+        standardizeSigns();
+    }
+
+    public boolean isLessThan(Rational_Wrapper comparitor) {
+        BigInteger balanced_curr_numerator = numerator.multiply(comparitor.denimonator);
+        BigInteger balanced_comp_numerator = comparitor.numerator.multiply(denimonator);
+        return balanced_curr_numerator.compareTo(balanced_comp_numerator) == -1; // returns true if the balanced
+                                                                                 // numerator of the current is greater
+                                                                                 // than the second
+    }
+
+    public boolean isGreaterThan(Rational_Wrapper comparitor) {
+        BigInteger balanced_curr_numerator = numerator.multiply(comparitor.denimonator);
+        BigInteger balanced_comp_numerator = comparitor.numerator.multiply(denimonator);
+        return balanced_curr_numerator.compareTo(balanced_comp_numerator) == 1; // returns true if the balanced
+                                                                                // numerator of the current is less than
+                                                                                // the second
+    }
+
+    public boolean equals(Rational_Wrapper comparitor) {
+        BigInteger balanced_curr_numerator = numerator.multiply(comparitor.denimonator);
+        BigInteger balanced_comp_numerator = comparitor.numerator.multiply(denimonator);
+        return balanced_curr_numerator.compareTo(balanced_comp_numerator) == 0; // returns true if the balanced
+                                                                                // numerator of the current is equal to
+                                                                                // the second
+    }
+
+    public static Rational_Wrapper multiply(Rational_Wrapper num1, Rational_Wrapper num2) {
+        BigInteger new_numerator = num1.numerator.multiply(num2.numerator);
+        BigInteger new_denominator = num1.denimonator.multiply(num2.denimonator);
+        // standardizeSigns(new_numerator, new_denominator);
+        Rational_Wrapper result = new Rational_Wrapper(new_numerator, new_denominator);
+        return result;
+    }
+
+    public static Rational_Wrapper divide(Rational_Wrapper numerator, Rational_Wrapper denimonator) {
+        BigInteger new_numerator = numerator.numerator.multiply(denimonator.denimonator);
+        BigInteger new_denominator = numerator.denimonator.multiply(denimonator.numerator);
+        // standardizeSigns(new_numerator, new_denominator);
+        Rational_Wrapper result = new Rational_Wrapper(new_numerator, new_denominator);
+        return result;
+    }
+
+    public static Rational_Wrapper add(Rational_Wrapper num1, Rational_Wrapper num2) {
+        BigInteger new_numerator = (num1.numerator.multiply(num2.denimonator))
+                .add(num2.numerator.multiply(num1.denimonator));
+        BigInteger new_denominator = num1.denimonator.multiply(num2.denimonator);
+        // standardizeSigns(new_numerator, new_denominator);
+        Rational_Wrapper result = new Rational_Wrapper(new_numerator, new_denominator);
+        // testing
+        // System.out.println("Addition: New numerator: " + new_numerator + " New
+        // denominator: " + new_denominator);
+        return result;
+    }
+
+    public static Rational_Wrapper subtract(Rational_Wrapper num1, Rational_Wrapper num2) {
+        BigInteger new_numerator = (num1.numerator.multiply(num2.denimonator))
+                .subtract(num2.numerator.multiply(num1.denimonator));
+        BigInteger new_denominator = num1.denimonator.multiply(num2.denimonator);
+        // standardizeSigns(new_numerator, new_denominator);
+        Rational_Wrapper result = new Rational_Wrapper(new_numerator, new_denominator);
+        // testing
+        // System.out.println("Addition: New numerator: " + new_numerator + " New
+        // denominator: " + new_denominator);
+        return result;
+    }
+
+    public void standardizeSigns() { // accesses data fields
+        // isn't needed as it will change the value of the
+        // original
+        int n_sign = 1;
+        int d_sign = 1;
+        if (numerator.compareTo(new BigInteger("0")) == -1) {
+            // smaller than 0
+            n_sign = -1;
+        }
+        if (denimonator.compareTo(new BigInteger("0")) == -1) {
+            // smaller than 0
+            d_sign = -1;
+        }
+
+        if (n_sign != d_sign) {
+            // imbalance
+            // make the numerator negative and the denominator positive - this might cause
+            // problems with 0 if it has a unique sign
+            numerator = numerator.multiply(new BigInteger(String.valueOf(n_sign))).multiply(new BigInteger("-1"));
+            denimonator = denimonator.multiply(new BigInteger(String.valueOf(d_sign)));
+        } else {
+            // if they have the same sign then the whole thing should be +ve
+            numerator = numerator.multiply(new BigInteger(String.valueOf(n_sign)));
+            denimonator = denimonator.multiply(new BigInteger(String.valueOf(d_sign)));
+        }
+
+        // testing
+        // System.out.println("Standardised: " + n + "/" + d);
     }
 
     public String toString() {
-        if (numerator.mod(denimonator).equals(new BigInteger("0"))) {
+        if (numerator.remainder(denimonator).equals(new BigInteger("0"))) {
             return (numerator.divide(denimonator)).toString();
-        } else if (numerator.compareTo(denimonator) == -1) {
+        } else if (numerator.abs().compareTo(denimonator.abs()) == -1) {
             // denominator is larger than numerator
             // therefore number is between 0-1
             return numerator.toString() + "/" + denimonator.toString();
         } else {
             // fractional number > 1
-            return numerator.divide(denimonator) + "." + numerator.mod(denimonator) + "/" + denimonator;
+            return numerator.divide(denimonator) + "." + numerator.remainder(denimonator).abs() + "/" + denimonator;
         }
     }
 
