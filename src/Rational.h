2fc3b7b4ccd566dba639e4cadaf417ce877c5140
Matt Tyler <tylma141@student.otago.ac.nz>
2023-05-19 08:57:21 +1200

    Added doxygen comments for Rational

--- src/Rational.h
+++ src/Rational.h
@@ -10,60 +10,108 @@
 
 namespace cosc326
 {
-
+	/**
+	 * @class Rational
+	 * @brief This class represents a Rational number, a ratio of two Integers.
+	 */
 	class Rational
 	{
 
 	public:
+		/**
+		 * @brief Default constructor
+		 */
 		Rational();
+		/**
+		 * @brief Constructs a Rational from a string
+		 * @param str The string to convert
+		 */
 		Rational(const std::string &str);
+		/**
+		 * @brief Copy constructor
+		 * @param r The Rational to copy
+		 */
 		Rational(const Rational &r);
-		Rational(const Integer &a);										// a
-		Rational(const Integer &a, const Integer &b);					// a/b
+		/**
+		 * @brief Constructs a Rational from an Integer
+		 * @param a The Integer
+		 */
+		Rational(const Integer &a);
+		/**
+		 * @brief Constructs a Rational from two Integers
+		 * @param a The numerator
+		 * @param b The denominator
+		 */
+		// a
+		Rational(const Integer &a, const Integer &b);
+		/**
+		 * @brief Constructs a Rational from three Integers
+		 * @param a The whole part
+		 * @param b The numerator of the fractional part
+		 * @param c The denominator of the fractional part
+		 */
+		// a/b
 		Rational(const Integer &a, const Integer &b, const Integer &c); // a + b/c
 
+		/**
+		 * @brief Destructor
+		 */
 		~Rational();
 
+		/**
+		 * @brief Assignment operator
+		 * @param r The Rational to assign
+		 */
 		Rational &operator=(const Rational &r); // q = r;
 
 		// Unary operators
-		Rational operator-() const; // -r;
-		Rational operator+() const; // +r;
+		Rational operator-() const; ///< Negation operator
+		Rational operator+() const; ///< Unary plus operator
 
 		// Arithmetic assignment operators
-		Rational &operator+=(const Rational &r); // q += r;
-		Rational &operator-=(const Rational &r); // q -= r;
-		Rational &operator*=(const Rational &r); // q *= r;
-		Rational &operator/=(const Rational &r); // q /= r;
+		Rational &operator+=(const Rational &r); ///< Addition assignment operator
+		Rational &operator-=(const Rational &r); ///< Subtraction assignment operator
+		Rational &operator*=(const Rational &r); ///< Multiplication assignment operator
+		Rational &operator/=(const Rational &r); ///< Division assignment operator
 
-		// lhs < rhs -- a 'friend' means < isn't a member, but can access the private parts of the class.
-		// You may need to make some other functions friends, but do so sparingly.
-		friend bool operator<(const Rational &lhs, const Rational &rhs);
+		friend bool operator<(const Rational &lhs, const Rational &rhs); ///< Less than operator
 
+		/**
+		 * @brief Gets the numerator of the Rational
+		 * @return The numerator
+		 */
 		Integer getNumerator() const;
+
+		/**
+		 * @brief Gets the denominator of the Rational
+		 * @return The denominator
+		 */
 		Integer getDenominator() const;
 
 	private:
-		Integer numerator;
-		Integer denominator;
+		Integer numerator;	 ///< The numerator of the Rational
+		Integer denominator; ///< The denominator of the Rational
 
+		/**
+		 * @brief Standardizes the signs of the numerator and denominator
+		 */
 		void standardizeSigns();
 	};
 
 	// Binary operators
-	Rational operator+(const Rational &lhs, const Rational &rhs); // lhs + rhs;
-	Rational operator-(const Rational &lhs, const Rational &rhs); // lhs - rhs;
-	Rational operator*(const Rational &lhs, const Rational &rhs); // lhs * rhs;
-	Rational operator/(const Rational &lhs, const Rational &rhs); // lhs / rhs;
+	Rational operator+(const Rational &lhs, const Rational &rhs); ///< Addition operator
+	Rational operator-(const Rational &lhs, const Rational &rhs); ///< Subtraction operator
+	Rational operator*(const Rational &lhs, const Rational &rhs); ///< Multiplication operator
+	Rational operator/(const Rational &lhs, const Rational &rhs); ///< Division operator
 
-	std::ostream &operator<<(std::ostream &os, const Rational &i); // std::cout << i << std::endl;
-	std::istream &operator>>(std::istream &is, Rational &i);	   // std::cin >> i;
+	std::ostream &operator<<(std::ostream &os, const Rational &i); ///< Stream insertion operator
+	std::istream &operator>>(std::istream &is, Rational &i);	   ///< Stream extraction operator
 
-	bool operator>(const Rational &lhs, const Rational &rhs);  // lhs > rhs
-	bool operator<=(const Rational &lhs, const Rational &rhs); // lhs <= rhs
-	bool operator>=(const Rational &lhs, const Rational &rhs); // lhs >= rhs
-	bool operator==(const Rational &lhs, const Rational &rhs); // lhs == rhs
-	bool operator!=(const Rational &lhs, const Rational &rhs); // lhs != rhs
+	bool operator>(const Rational &lhs, const Rational &rhs);  ///< Greater than operator
+	bool operator<=(const Rational &lhs, const Rational &rhs); ///< Less than operator or equal to
+	bool operator>=(const Rational &lhs, const Rational &rhs); ///< Greater than operator or equal to
+	bool operator==(const Rational &lhs, const Rational &rhs); ///< Equal to operator
+	bool operator!=(const Rational &lhs, const Rational &rhs); ///< Not equal to operator
 
 }
 
